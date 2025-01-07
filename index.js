ac39aa28f78c0a59641c1c2349365d0e72eb73e8
Henrique Lacerda <henriquelacerda-2013@hotmail.com>
2025-01-07 16:06:54 -0300

    Finalizado

--- index.js
+++ index.js
@@ -8,27 +8,54 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-io.on('connection', (socket) => {
-    console.log(`Um usuário com o id ${socket.id} conectou ao servidor`)
-    socket.data.nickname = socket.id.substring(0, 5)
+function sortConnectedUsersByNickname(connectedUsers) {
+    const entries = Array.from(connectedUsers.entries())
+    entries.sort((a, b) => a[1].localeCompare(b[1]))
+    
+    return new Map(entries)
+}
 
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} desconectou do servidor`)
-    })
+const connectedUsers = new Map()
+
+io.on('connection', (socket) => {
+
+    console.log(`Um usuário com o id ${socket.id} conectou ao servidor`)
+    
+    socket.on('changeNickname', (nickname) => {
+        socket.data.nickname = nickname
+        connectedUsers.set(socket.id, nickname)
+        io.emit('changeNickname', nickname)
+        const sortedUserList = sortConnectedUsersByNickname(connectedUsers)
+        io.emit('existingUsers', Array.from(sortedUserList.values()))
+        console.log("connecteds", connectedUsers)
+    });
+
+    socket.on('getExistingUsers', () => {
+        const sortedUsers = sortConnectedUsersByNickname(connectedUsers)
+        const userList = Array.from(sortedUsers.values())
+        socket.emit('existingUsers', userList)
+    });
 
     socket.on('chat', (msg) => {
-        console.log(`${socket.id} escreveu ${msg}`)
         io.emit('chat', msg, socket.data.nickname)
-    })
+    });
 
-    socket.on('trocaNickname', (nickname) => {
-        socket.data.nickname = nickname
-    })
-})
+    socket.on('disconnect', () => {
+        const disconnectedNickname = connectedUsers.get(socket.id)
+        connectedUsers.delete(socket.id)
+        if (disconnectedNickname) {
+            io.emit('userDisconnected', disconnectedNickname)
+        }
+        console.log("desconnecteds", connectedUsers)
+    });
+
+});
 
 server.listen(port, () => {
     console.log(`Servidor rodando...`)
---
+++ public/js/script.js
@@ -0,0 +1,105 @@
+const socket = io();
+const nicknameInput = document.getElementById("nickname")
+const messageInput = document.getElementById("message-input")
+const sendButton = document.getElementById("send-button")
+const messagesDisplay = document.getElementById("messages-display")
+const userNames = document.getElementById("user-names")
+const leaveButton = document.getElementById("leave")
+
+document.getElementById("message-input-area").style.visibility = "hidden"
+let userList = []
+
+function hiddenNickname () {
+    const nickname = document.getElementById("nickname").value
+
+    if (nickname.trim() !== "") {
+        socket.emit("changeNickname", nickname)
+        document.getElementById("nickname-form-hidden").style.display = "none"
+        document.getElementById("message-input-area").style.visibility = "visible"
+    } else {
+        const error = document.getElementById("error-nickname").textContent = "Por favor, digite um apelido."
+    }
+}
+
+function addUserToList(nickname) {
+    const userNamesElement = document.createElement('li')
+    userNamesElement.innerHTML = nickname
+    userNames.appendChild(userNamesElement)
+    userNames.scrollTop = userNames.scrollHeight
+}
+
+function displayMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.classList.add('message')
+    messageElement.innerHTML = message
+    messagesDisplay.appendChild(messageElement)
+    messagesDisplay.scrollTop = messagesDisplay.scrollHeight
+}
+
+function removeUserFromList(nickname) {
+    const userListItems = document.querySelectorAll('#user-names li');
+    userListItems.forEach(item => {
+        if (item.textContent === nickname) {
+            item.remove()
+        }
+    });
+}
+
+function sendMessage() {
+    const message = messageInput.value;
+    if (message.trim() !== "") {
+        socket.emit('chat', message);
+        messageInput.value = "";
+    }
+}
+
+function leaveChat() {
+    socket.disconnect()
+    window.location.reload()
+}
+
+function updateUIWithUserList(userList) {
+    userNames.innerHTML = ""
+    userList.forEach((user) => {
+        addUserToList(user)
+    });
+}
+
+socket.on('connect', () => {
+    socket.emit('getExistingUsers')
+})
+
+socket.on('existingUsers', (initialUserList) => {
+    userList = initialUserList
+    updateUIWithUserList(userList)
+})
+
+socket.on('existingUsers', (updatedUserList) => {
+    userList = updatedUserList
+    updateUIWithUserList(userList)
+})
+
+sendButton.addEventListener("click", sendMessage)
+
+messageInput.addEventListener("keypress", (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault()
+        sendMessage()
+    } else if (event.key === 'Enter' && event.shiftKey) {
+
+    }
+})
+
+socket.on('chat', (msg, nickname) => {
+    displayMessage(`<span class="username">${nickname}: </span>${msg}`)
+})
+
+socket.on('changeNickname', (nickname) => {
+    displayMessage(`<span class="username">${nickname} </span>entrou na sala...`)
+    addUserToList(nickname)
+})
+
+socket.on('userDisconnected', (nickname) => {
+    removeUserFromList(nickname)
+    displayMessage(`<span class="username">${nickname} </span>saiu da sala...`)
+});
